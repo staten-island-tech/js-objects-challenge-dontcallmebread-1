9e7e554048dad37deee206e708dd8651c4bd66a9
dontcallmebread <57456875+dontcallmebread@users.noreply.github.com>
2020-04-06 10:44:26 -0400

    i give up, read notes

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ Homer.family.faughter = {
 };
 
 const { name, age} = Riya; */
-
+/* 
 const presidents = [
     { name: "Donald Trump", terms: 1, party: "R" },
     { name: "Barack Obama", terms: 2, party: "D" },
@@ -56,28 +56,63 @@ const presidents = [
     { name: "John F. Kennedy", terms: 1, party: "D" },
     { name: "Lyndon Johnson", terms: 2, party: "D" },
     { name: "Dwight Eisenhower", terms: 2, party: "R" }
-  ];
+  ];  */
    
+//NOTE! ! ! (need to block const presidents and use var when looking at #2 and #4,
+//and also block the code for the question when you're looking at the other one
+//I really tried and this is the best I could do T_T)
+
   //Challenge Questions
   //1) Filter all presidents, leaving only the Democratic ones
-   const Dems = presidents.filter(president => president.party === "D"); 
+ /*   const Dems = presidents.filter(president => president.party === "D");  */
 
   //2)Filter all presidents to leave only one term Republican presidents HINT use If statement
-   let Reps = presidents.filter(president => president.party === "R");
-   let One = presidents.filter(president => president.terms === 1);
 
+var presidents = [
+    { name: "Donald Trump", terms: 1, party: "R" },
+    { name: "Barack Obama", terms: 2, party: "D" },
+    { name: "George W. Bush", terms: 2, party: "R" },
+    { name: "Bill Clinton", terms: 2, party: "D" },
+    { name: "George H.W. Bush", terms: 1, party: "R" },
+    { name: "Ronald Reagan", terms: 2, party: "R" },
+    { name: "Jimmy Carter", terms: 1, party: "D" },
+    { name: "Gerlad Ford", terms: 1, party: "R" },
+    { name: "Richard Nixon", terms: 2, party: "R" },
+    { name: "John F. Kennedy", terms: 1, party: "D" },
+    { name: "Lyndon Johnson", terms: 2, party: "D" },
+    { name: "Dwight Eisenhower", terms: 2, party: "R" }
+  ]; 
+  var filter = {
+    terms: 1, 
+    party: "R"
+}; 
 
-
-/*     let oneRepublican = [];
-    for (let i = 0; i < presidents.length; i++){
-        if (presidents[i].terms < 2){
-            oneRepublican.push(presidents[i]);
-        }
-    } */
+ presidents = presidents.filter(function(item) {
+    for (var key in filter) {
+        if (item[key] === undefined || item[key] != filter[key])
+            return false;
+    }
+    return true;
+}); 
+console.log(presidents);
 
   //3) return only the last three presidents
- console.log(presidents.slice(-3)) 
+  console.log(presidents.slice(-3))  
    
   //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+/*   var filter = {
+    terms: 2, 
+    party: "D"
+  }
+
+presidents = presidents.filter(function(item) {
+    for (var key in filter) {
+        if (item[key] === undefined || item[key] != filter[key])
+            return false;
+    }
+    return true;
+});
+
+console.log(presidents); */
    
   //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
